ccdb9147cce089486fc70ae4e57695135de14c30
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-03-27 23:38:43 +0200

    reste : langue/vague/config anim mousescroll

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,44 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 0,
+    spaceBetween: 10,
     loop: true,
-    autoplay: {
-        delay: 2000,
+    
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-});
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+})
+
+
+ScrollReveal().reveal('.headline');
+
+if (document.querySelector(".project")) {
+    window.addEventListener("scroll", function() {     
+        const mouseScroll = document.querySelector(".ask__scroll");
+        let opacity = 1;
+        const delay = 1000;
+        const interval = 50;
+        const fadeOut = setInterval(() => {
+            opacity -= 0.1;
+            mouseScroll.style.opacity = opacity;
+            if (opacity <= 0) {
+                clearInterval(fadeOut);
+                mouseScroll.style.display = 'none';
+            }
+        }, interval);
+        setTimeout(() => {
+            fadeOut;
+        }, delay);
+    });
+}
