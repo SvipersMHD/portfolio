c9fd0c6ccc2391daafa1ab029a1ad0f39f9b4d93
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-03-28 23:08:52 +0200

    full responsive, reste photo et img screen

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,6 @@
 const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
     loop: true,
-    
-    
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -20,25 +18,109 @@ const swiper = new Swiper('.swiper', {
     }
 })
 
+const askScroll = document.querySelector('.ask__scroll');
+const wavesL = [...document.querySelectorAll('.wave__left div')].reverse();
+const wavesR = document.querySelectorAll('.wave__right div');
 
-ScrollReveal().reveal('.headline');
+const btnProjectHome = document.querySelector('.home__project');
+const btnContact = document.querySelector('.home__contact');
 
-if (document.querySelector(".project")) {
-    window.addEventListener("scroll", function() {     
-        const mouseScroll = document.querySelector(".ask__scroll");
-        let opacity = 1;
-        const delay = 1000;
-        const interval = 50;
-        const fadeOut = setInterval(() => {
-            opacity -= 0.1;
-            mouseScroll.style.opacity = opacity;
-            if (opacity <= 0) {
-                clearInterval(fadeOut);
-                mouseScroll.style.display = 'none';
-            }
-        }, interval);
-        setTimeout(() => {
-            fadeOut;
-        }, delay);
+const project = document.querySelector('.project');
+const home = document.querySelector('.home');
+const header = document.querySelector('.header');
+const homebtn = document.querySelector('.project__homebtn');
+
+window.addEventListener('scroll', () => {
+    
+    if (document.querySelector(".project")) {
+        askScroll.classList.add('isActive')
+    }
+    setTimeout(function() {
+        askScroll.style.opacity = 0;
+    }, 1000);
+});
+
+
+function goProject(){
+    setTimeout(function() {
+        project.style.display = "block";
+        home.style.display = "none";
+        header.style.display = "none";
+    }, 1000);
+}
+
+function goHome(){
+    setTimeout(function() {
+        project.style.display = "none";
+        home.style.display = "flex";
+        header.style.display = "block";
+    }, 900);
+}
+
+btnProjectHome.addEventListener("click", () => {
+    animation();
+    goProject();
+});
+
+btnContact.addEventListener("click", () => {
+    animation();
+});
+
+homebtn.addEventListener("click", () => {
+    animation();
+    goHome()
+    askScroll.classList.remove('isActive')
+    setTimeout(function() {
+        askScroll.style.opacity = 1;
+    }, 1000);
+});
+
+
+
+function animation() {
+    gsap.to(wavesR, {
+        xPercent: -100, // Animer la position à 0 pour chaque élément
+        duration: 0.5, // Durée de l'animation
+        stagger: 0.1, // Décalage entre chaque élément
+        ease: "power2.out" // Courbe d'animation
+    });
+    
+    gsap.to(wavesL, {
+        xPercent: 100, // Animer la position à 0 pour chaque élément
+        duration: 0.5, // Durée de l'animation
+        stagger: 0.1, // Décalage entre chaque élément
+        ease: "power2.out", // Courbe d'animation
+        onComplete: function() {
+            gsap.to(wavesR, {
+                delay: .3,
+                xPercent: -200, // Animer la position à 0 pour chaque élément
+                duration: 0.5, // Durée de l'animation
+                stagger: 0.1, // Décalage entre chaque élément
+                ease: "power2.out" // Courbe d'animation
+            });
+            
+            gsap.to(wavesL, {
+                delay: .3,
+                xPercent: 200, // Animer la position à 0 pour chaque élément
+                duration: 0.5, // Durée de l'animation
+                stagger: 0.1, // Décalage entre chaque élément
+                ease: "power2.out", // Courbe d'animation
+                onComplete: function() {
+                    // Remettre les éléments à leur position de départ
+                    gsap.set(wavesR, {
+                        xPercent: 0,
+                        right: '-100%'
+                    });
+                    
+                    gsap.set(wavesL, {
+                        xPercent: 0,
+                        left: '-100%'
+                    });
+                }
+            });
+        }
     });
 }
+
+
+
